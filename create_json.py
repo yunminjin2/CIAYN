0381daa1f24ebc7a60c31dc21dc67085c335b742
Minje Kim <yunminjin2@gmail.com>
2023-05-22 12:56:26 +0900

    upload

--- create_json.py
+++ create_json.py
@@ -2,7 +2,7 @@ import json
 import os
 import random
 
-FILE_PATH =  os.path.join('json', 'GwangGuo', 'GW001.json')
+FILE_PATH =  os.path.join('json', 'Koorong', 'KO001.json')
 
 
 class Car:
@@ -10,7 +10,7 @@ class Car:
         self.ip = self.create_ip()
         self.gps = gps
         self.gps[0] += 37.
-        self.gps[1] += 127.
+        self.gps[1] += 126.
 
     def create_ip(self):
         return '.'.join([str(random.randint(0, 256)) for i in range(4)])
@@ -22,23 +22,17 @@ if __name__ == '__main__':
 
     cars = []
 
-    cars.append(Car(gps=[.285331, .056075]))    
-    cars.append(Car(gps=[.285334, .055901]))    
-    cars.append(Car(gps=[.285212, .056000]))    
-    cars.append(Car(gps=[.285106, .055836]))    
-    cars.append(Car(gps=[.285182, .055781]))    
-    cars.append(Car(gps=[.285255, .055682]))    
-    cars.append(Car(gps=[.285287, .055636]))    
-    cars.append(Car(gps=[.285293, .055680]))    
-    cars.append(Car(gps=[.285301, .055726]))    
-    cars.append(Car(gps=[.285322, .055700]))    
-    cars.append(Car(gps=[.285343, .055677]))    
-    cars.append(Car(gps=[.285310, .055630]))    
-    cars.append(Car(gps=[.285428, .055662]))    
-    cars.append(Car(gps=[.285375, .055875]))    
-    cars.append(Car(gps=[.285422, .055931]))    
-    cars.append(Car(gps=[.285465, .055830]))    
-    cars.append(Car(gps=[.285587, .055504]))    
+    cars.append(Car(gps=[.423727, .993252]))    
+    cars.append(Car(gps=[.423954, .993203]))    
+    cars.append(Car(gps=[.423944, .993237]))    
+    cars.append(Car(gps=[.423870, .993272]))    
+    cars.append(Car(gps=[.423957, .993251]))    
+    cars.append(Car(gps=[.423907, .993341]))    
+    cars.append(Car(gps=[.423935, .993372]))    
+
+
+
+
 
     result = dict(zip(range(len(cars)), [ob.__dict__ for ob in cars]))
 
